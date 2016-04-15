c02e181568c9bcb5f9543a3235344c744c92c6c5
Richard Dalton <richard@devjoy.com>
2016-04-15 16:39:57 +0100

    Fixed Import

--- ReusableAuth/forms.py
+++ ReusableAuth/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from accounts.models import User
+from models import User
 from django.core.exceptions import ValidationError
 
 
